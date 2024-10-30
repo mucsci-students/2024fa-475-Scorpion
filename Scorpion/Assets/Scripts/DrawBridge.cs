22645cd1c4a044625ba04a4f49175ebb64f7bbcb
DavoMar <156874527+DavoMar@users.noreply.github.com>
2024-10-30 03:10:17 -0400

    Drawbridge script and prefab

---
+++ Scorpion/Assets/Scripts/DrawBridge.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DrawBridge : MonoBehaviour
+{
+    public Sprite closedSprite;
+    //public Sprite closedSpritetwo;
+    public Sprite openSprite;
+    //public Sprite openSpritetwo;
+    private SpriteRenderer spriteRenderer;
+    private bool isOpen = false;
+    public Pressureplate pressurePlate;
+    public BoxCollider2D doorCollider;
+
+    void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        spriteRenderer.sprite = closedSprite;
+        doorCollider = GetComponent<BoxCollider2D>();
+    }
+
+    void Update()
+    {
+        if (pressurePlate != null && pressurePlate.isitActive())
+        {
+            if (!isOpen)
+            {
+                isOpen = true;
+                spriteRenderer.sprite = openSprite;
+                doorCollider.enabled = true; // Disable the collider to allow passage
+            }
+        }
+        else
+        {
+            if (isOpen)
+            {
+                isOpen = false;
+                spriteRenderer.sprite = closedSprite;
+                doorCollider.enabled = false; // Enable the collider to block passage
+            }
+        }
+    }
+}
