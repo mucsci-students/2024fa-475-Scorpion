861feb90287d90e3cf9f3cbf45723fc0461317ce
DavoMar <156874527+DavoMar@users.noreply.github.com>
2024-11-04 10:48:58 -0500

    Camera updates

--- Scorpion/Assets/Scripts/CameraMov.cs
+++ Scorpion/Assets/Scripts/CameraMov.cs
@@ -5,17 +5,22 @@ using UnityEngine;
 public class CameraMov : MonoBehaviour
 {
     public bool moving = false;     //To be used later for stop and go implementation
-    public float camSpeed = 0.5f;
-    public float boundHeight = 50f;
-    public CamTrigger camTrigger;
-    private bool inContact = false;
-
+    public float camSpeed = 1f;
+    public float boundHeight = 200f;
+    public CamTrigger startOne;
+    public CamTrigger startTwo;
+    //public CamTrigger startThree;
+    public CamTrigger stopOne;
+    //public CamTrigger stopTwo;
+    //public CamTrigger stopThree;
+    private bool startContact = false;
+    private bool stopContact = false;
+    
     // Start is called before the first frame update
     void Start()
     {
         
     }
-
 
     void startMoving(){
         moving = true;
@@ -32,19 +37,33 @@ public class CameraMov : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        inContact = camTrigger.moveCam();
+        if (startOne.moveCam() || startTwo.moveCam() /*|| startThree.moveCam()*/){
+        startContact = true;
+        stopContact = false;
+        }
+        
+        if(startContact == true){
+            startMoving();
+        }
+
+        if (stopOne.moveCam() /*|| stopTwo.moveCam() || stopThree.moveCam()*/){
+        stopContact = true;
+        startContact = false;
+        }
+
+        if (stopContact == true){
+            stopMoving();
+        }
+
+        if(moving == true){
         Vector3 newPostion = transform.position + Vector3.up * camSpeed * Time.deltaTime;
         if (newPostion.y <= boundHeight && moving){
             transform.position = newPostion;
         }
-        //if(Input.GetMouseButtonDown(0)){
-        /*if(inContact == false){
-            stopMoving();
-        }*/
-        //if(Input.GetKeyDown(KeyCode.Space)){
-        if(inContact == true){
-            startMoving();
         }
+        
+        //stopContact = false;
+        //startContact = false;
 
     }
 }
