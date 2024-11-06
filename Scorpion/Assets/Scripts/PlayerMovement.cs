4b5902022021f7a05e6256e8b4c6dff3c9c04780
DavoMar <156874527+DavoMar@users.noreply.github.com>
2024-11-06 01:48:20 -0500

    Player 1 and 2 animations and player movement changes

--- Scorpion/Assets/Scripts/PlayerMovement.cs
+++ Scorpion/Assets/Scripts/PlayerMovement.cs
@@ -17,6 +17,7 @@ public class PlayerMovement : MonoBehaviour
     public KeyCode leftButton = KeyCode.A;
     public KeyCode rightButton = KeyCode.D;
     public bool isAttacking = false;
+    public Animator animator;
 
     private Rigidbody2D rb;
     [HideInInspector]
@@ -40,34 +41,73 @@ public class PlayerMovement : MonoBehaviour
     {
         Vector2 movement = Vector2.zero;
 
-        if (Input.GetKey(upButton))
-            movement.y += 1;
-        if (Input.GetKey(downButton))
-            movement.y -= 1;
-        if (Input.GetKey(leftButton))
-            movement.x -= 1;
-        if (Input.GetKey(rightButton))
-            movement.x += 1;
+        if (Input.GetKey(upButton)) movement.y += 1;
+        if (Input.GetKey(downButton)) movement.y -= 1;
+        if (Input.GetKey(leftButton)) movement.x -= 1;
+        if (Input.GetKey(rightButton)) movement.x += 1;
 
         if (movement != Vector2.zero)
         {
             movement.Normalize();
             rb.velocity = movement * moveSpeed;
-
-            // Update lastFacingDirection every frame if there's movement
             lastFacingDirection = movement;
 
-            // Flip the player only on the X-axis when moving left or right
+            HandleAnimations(movement);
+
             if (movement.x != 0)
             {
-                rend.flipX = movement.x < 0;
+                rend.flipX = movement.x > 0;
             }
         }
         else
         {
             rb.velocity = Vector2.zero;
+            HandleIdleAnimations();
         }
     }
+
+    void HandleAnimations(Vector2 movement)
+    {
+        animator.SetBool("moveUp", false);
+        animator.SetBool("moveDown", false);
+        animator.SetBool("moveSide", false);
+        animator.SetBool("moveUpRight", false);
+        animator.SetBool("moveUpLeft", false);
+        animator.SetFloat("moveSpeed", 1);
+
+        if (movement.y > 0 && movement.x > 0)
+        {
+            animator.SetBool("moveUpRight", true);
+        }
+        else if (movement.y > 0 && movement.x < 0)
+        {
+            animator.SetBool("moveUpLeft", true);
+        }
+        else if (movement.y > 0)
+        {
+            animator.SetBool("moveUp", true);
+        }
+        else if (movement.y < 0)
+        {
+            animator.SetBool("moveDown", true);
+        }
+        else if (movement.x != 0)
+        {
+            animator.SetBool("moveSide", true);
+        }
+    }
+
+    void HandleIdleAnimations()
+    {
+        animator.SetBool("moveUp", false);
+        animator.SetBool("moveDown", false);
+        animator.SetBool("moveSide", false);
+        animator.SetBool("moveUpRight", false);
+        animator.SetBool("moveUpLeft", false);
+        animator.SetFloat("moveSpeed", 0);
+    }
+
+
 
     // Method to apply a permanent speed power-up
     public void ApplyPermanentSpeedBoost(float speedMultiplier)
