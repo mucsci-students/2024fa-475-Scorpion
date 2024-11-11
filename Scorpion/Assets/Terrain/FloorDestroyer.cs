c426018c92823924665cf2ffeea82977de3dbb9f
Lincoln <lincolncraddock@gmail.com>
2024-11-11 01:44:33 -0500

    added pitfalls, etc to Copy of Main

--- Scorpion/Assets/Terrain/FloorDestroyer.cs
+++ Scorpion/Assets/Terrain/FloorDestroyer.cs
@@ -8,12 +8,16 @@ public class FloorDestroyer : MonoBehaviour
     [SerializeField] private int leftmostTileX; // the x pos of the leftmost tile to destroy
     [SerializeField] private int rightmostTileX; // the x pos of the rightmost tile
     [SerializeField] private int nextRowY; // the y pos of the next row to destroy (starting with the 1st)
+    [SerializeField] private List<int> immuneRanges; // ranges of y values that cannot be destroyed (for shops)
     [SerializeField] private GameObject fallingTilePrefab;
     [SerializeField] private GameObject lavaLightPrefab;
     [SerializeField] private Camera cam;
 
     private float timeOfLastDestroy = 0f;
     private float timeBetweenDestroys = 0.2f;
+    private int currImmuneRangeMin;
+    private int currImmuneRangeMax;
+    private int immuneRangeIndex = 0;
 
     private Tilemap floor;
     private float destroyRadius; // how far behind the camera must be to destroy tiles
@@ -25,6 +29,8 @@ public class FloorDestroyer : MonoBehaviour
         floor = GetComponent<Tilemap> ();
         destroyRadius = -cam.transform.position.z / 5f * 6f - 2f; // destroyRadius depends on the camera's z pos
         fallingTilePrefab.GetComponent<FallingTile> ().wobbleDuration = 1f / cam.GetComponent<CameraMov> ().camSpeed; // the tile's time spent wobbling depends on the camera's speed
+        currImmuneRangeMin = immuneRanges[immuneRangeIndex++];
+        currImmuneRangeMax = immuneRanges[immuneRangeIndex++];
     }
 
     void Update()
@@ -33,30 +39,45 @@ public class FloorDestroyer : MonoBehaviour
         // continuously destroy rows of tiles as the camera moves
         if ((float) nextRowY + destroyRadius < cam.transform.position.y)
         {
-            // destroy all remaining tiles in the last row
-            DestroyTiles (locToDestroySoon);
-            // add a new row of tiles to destroy
-            locToDestroySoon = new List<Vector2> ();
-            for (int x = leftmostTileX; x <= rightmostTileX; ++x)
+            if (nextRowY <= currImmuneRangeMin)
             {
-                locToDestroySoon.Add (new Vector2 (x, nextRowY));
+                // destroy all remaining tiles in the last row
+                DestroyTiles (locToDestroySoon);
+                // add a new row of tiles to destroy
+                locToDestroySoon = new List<Vector2> ();
+                for (int x = leftmostTileX; x <= rightmostTileX; ++x)
+                {
+                    locToDestroySoon.Add (new Vector2 (x, nextRowY));
+                }
+            }
+            else if (nextRowY > currImmuneRangeMax)
+            {
+                currImmuneRangeMin = immuneRanges[immuneRangeIndex++];
+                currImmuneRangeMax = immuneRanges[immuneRangeIndex++];
             }
             nextRowY += 1;
         }
         else if (timeOfLastDestroy + timeBetweenDestroys < Time.time)
         {
-            // destroy a random few tiles in the current row
-            List<Vector2> locations = new List<Vector2> ();
-            int n = Random.Range (1, 4);
-            for (int i = 0; i < n && locToDestroySoon.Count > 0; ++i)
+            if (nextRowY <= currImmuneRangeMin)
             {
-                int j = Random.Range (0, locToDestroySoon.Count);
-                locations.Add (locToDestroySoon[j]);
-                locToDestroySoon.RemoveAt(j);
+                // destroy a random few tiles in the current row
+                List<Vector2> locations = new List<Vector2> ();
+                int n = Random.Range (1, 4);
+                for (int i = 0; i < n && locToDestroySoon.Count > 0; ++i)
+                {
+                    int j = Random.Range (0, locToDestroySoon.Count);
+                    locations.Add (locToDestroySoon[j]);
+                    locToDestroySoon.RemoveAt(j);
+                }
+                DestroyTiles (locations);
+                timeOfLastDestroy = Time.time;
             }
-            DestroyTiles (locations);
-            timeOfLastDestroy = Time.time;
-            
+            else if (nextRowY > currImmuneRangeMax)
+            {
+                currImmuneRangeMin = immuneRanges[immuneRangeIndex++];
+                currImmuneRangeMax = immuneRanges[immuneRangeIndex++];
+            }
         }
         
     }
